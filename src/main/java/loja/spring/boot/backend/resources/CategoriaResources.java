5b722ba0be521f599800b40b4e8b4703f39de01e
Lucas <monteiro9961@gmail.com>
2023-03-08 11:17:32 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/loja/spring/boot/backend/resources/CategoriaResources.java
+++ src/main/java/loja/spring/boot/backend/resources/CategoriaResources.java
@@ -1,15 +1,25 @@
 package loja.spring.boot.backend.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import loja.spring.boot.backend.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Testando o Rest";
-	}
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
+		}
 }
