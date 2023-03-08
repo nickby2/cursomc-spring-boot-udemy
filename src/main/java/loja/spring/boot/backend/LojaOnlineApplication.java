82f315592ebd06551e39b12a0de068e8201299e6
Lucas <monteiro9961@gmail.com>
2023-03-08 11:02:14 -0300

    REST testado

---
+++ src/main/java/loja/spring/boot/backend/LojaOnlineApplication.java
@@ -0,0 +1,13 @@
+package loja.spring.boot.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaOnlineApplication.class, args);
+	}
+
+}
